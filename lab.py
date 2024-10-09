a44b9798a0ca70625cf8100855373f21c5f7afd3
oToTak <t.teodorko@gmail.com>
2024-10-09 13:32:25 +0300

    def

--- lab.py
+++ lab.py
@@ -1,23 +1,23 @@
-File = open(r"test_csv.csv", "r")
+File = open(r"lab_discrete_math1/test_csv.csv", "r")
 
 content = File.read()
 lines = content.splitlines()
-matrix = [line.split(',') for line in lines]
+matrix0 = [line.split(',') for line in lines]
+def reading(matrix):
+    '''
+    
+    '''
+    a = 0
+    b = -1
+    lst = []
+    while a < 5:
+        b += 1
+        tesotva = matrix[a][b]
+        if matrix[a][b] == '"1"':
+            lst.append([a + 1, b + 1])
+        if b == 4:
+            b = -1
+            a += 1
+    return lst
 
-a = 0
-b = -1
-lst = []
-while a < 5:
-    b += 1
-    tesotva = matrix[a][b]
-    if matrix[a][b] == '"1"':
-        lst.append([a + 1, b + 1])
-    if b == 4:
-        b = -1
-        a += 1
-
-print(matrix)
-
-print(lst)
-
-# jsjsh
+print(reading(matrix0))
