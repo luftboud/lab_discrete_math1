8e192ba5732a02738db7f453992f93e992dfef80
oToTak <t.teodorko@gmail.com>
2024-10-09 13:27:18 +0300

    .

---
+++ lab.py
@@ -0,0 +1,21 @@
+File = open(r"test_csv.csv", "r")
+
+content = File.read()
+lines = content.splitlines()
+matrix = [line.split(',') for line in lines]
+
+a = 0
+b = -1
+lst = []
+while a < 5:
+    b += 1
+    tesotva = matrix[a][b]
+    if matrix[a][b] == '"1"':
+        lst.append([a + 1, b + 1])
+    if b == 4:
+        b = -1
+        a += 1
+
+print(matrix)
+
+print(lst)
---
+++ tesovy.py
@@ -0,0 +1,6 @@
+lst = []
+
+for i in range(10):
+    lst.append([i, 0])
+
+print(lst)
